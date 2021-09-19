c14dce0c63b2c8d3df3ad2253cffd4c155d46668
Code Sandbox <hello@codesandbox.io>
2021-09-19 08:29:10 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.737f749c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpemoji-world"]=this["webpackJsonpemoji-world"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),o=n(3),i=n.n(o),r=(n(8),n(0));function s(){return Object(r.jsxs)("div",{className:"App",children:[Object(r.jsx)("h1",{children:"Hello CodeSandbox"}),Object(r.jsx)("h2",{children:"Start editing to see some magic happen!"})]})}var d=document.getElementById("root");i.a.render(Object(r.jsx)(c.StrictMode,{children:Object(r.jsx)(s,{})}),d)},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.737f749c.chunk.js.map
